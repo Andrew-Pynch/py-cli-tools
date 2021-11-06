6d2b72a65ff3a3994341ef207644032c057bf9f6
Andrew Pynch <andrewpynchbusiness@gmail.comcode>
2021-11-06 16:40:54 -0500

    update version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="py-cli-tools",
-    version="1.0.0",
+    version="1.0.1",
     description="Functions that I found myself writing in different projects over and over again go here",
     long_description=README,
     long_description_content_type="text/markdown",
